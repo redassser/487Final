40ad1fe64e44acaece45526c0e0c0f62974bc69f
Bryan-Feighner <98185398+Bryan-Feighner@users.noreply.github.com>
2023-12-16 16:32:45 -0500

    Working

--- brick_maker.vhd
+++ brick_maker.vhd
@@ -3,7 +3,7 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-ENTITY brick_maker IS
+ENTITY brickmaker IS
     PORT (
         v_sync : IN STD_LOGIC;
         pixel_row : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
@@ -20,21 +20,22 @@ ENTITY brick_maker IS
         ball_speed : IN STD_LOGIC_VECTOR (10 DOWNTO 0);
         brick_on : OUT STD_LOGIC
     );
-END brick_maker;
+END brickmaker;
 
-ARCHITECTURE Behavioral of brick_maker IS
+ARCHITECTURE Behavioral of brickmaker IS
     SIGNAL brick_active : STD_LOGIC := '0';
+    SIGNAL once : STD_LOGIC := '0';
     BEGIN
-    brickdraw : PROCESS (pixel_col, pixel_row) IS
+    brickdraw : PROCESS(pixel_col, pixel_row) IS
     BEGIN
-        IF (pixel_col >= left_b)  AND 
-           (pixel_col < right_b)  AND 
-           (pixel_row >= top_b)   AND 
+        IF (pixel_col >= left_b) AND 
+           (pixel_col < right_b) AND 
+           (pixel_row >= top_b) AND 
            (pixel_row < bottom_b) AND
-           (brick_active = '1')   THEN
-            brick_on <= '0';
-        ELSE
+           (brick_active = '1') THEN
             brick_on <= '1';
+        ELSE
+            brick_on <= '0';
         END IF;
     END PROCESS;
 
@@ -43,9 +44,15 @@ ARCHITECTURE Behavioral of brick_maker IS
         WAIT UNTIL rising_edge(v_sync);
         IF serve = '1' AND game_on = '0' THEN -- Start game
             brick_active <= '1';
+            ball_bounce_b <= '0';
+            once <= '0';
+        ELSIF once = '1' then
+            ball_bounce_b <= '0';    
         ELSIF (ball_x >= left_b AND ball_x <= right_b) AND
               (ball_y >= top_b AND ball_y <= (bottom_b + 5)) THEN -- Bounce off bottom side
             ball_bounce_b <= '1';
+            once <= '1';
+            brick_active <= '0';
         --ELSIF () -- Bounce off top side
         --ELSIF () -- Bounce off left side
         --ELSIF () -- Bounce off right side
